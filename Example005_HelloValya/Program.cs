e810a4140de54613edbdf7040d8e2d6d47dd9e1c
Aleksandr Malikov <HeadOfTheFather@gmail.com>
2022-12-13 19:26:38 +0300

    add a Hello

---
+++ Example005_HelloValya/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Write your name: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "valya")
+{
+    Console.WriteLine("Hello my lovely, Valen`ka!");
+}
+else
+{
+    Console.Write("Hello,");
+    Console.WriteLine(username);  
+}
